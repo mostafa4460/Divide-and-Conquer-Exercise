0cadcfa17731f429d228f41c3687ffff9f10b53f
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-06-13 00:40:03 -0400

    countZeroes & sortedFrequency

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,19 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+    if (arr[0] === 0) return arr.length;
+    else if (arr[arr.length - 1] !== 0) return 0;
+
+    let leftIndx = 0,
+        rightIndx = arr.length - 1;
+
+    while (arr[rightIndx] === 0) {
+        let middleIndx = Math.floor((leftIndx + rightIndx) / 2);
+        if (arr[middleIndx] !== 0) {
+            leftIndx = middleIndx + 1;
+        } else {
+            rightIndx = middleIndx - 1;
+        }
+    }
+    return arr.length - rightIndx - 1;
 }
 
 module.exports = countZeroes
--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,42 @@
-function sortedFrequency() {
+function sortedFrequency(arr, value) {
+    let first = findFirst(arr, value);
+    if (first === -1) return first;
+    let last = findLast(arr, value);
+    return last - first + 1;
+}
 
+function findFirst(arr, value) {
+    if (arr[0] === value) return 0;
+    
+    let low = 0,
+        high = arr.length - 1;
+    while (high >= low) {
+        let mid = Math.floor((low + high) / 2);
+        if (value > arr[mid]) {
+            low = mid + 1;
+        } else {
+            if (value === arr[mid] && value > arr[mid - 1]) return mid;
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+function findLast(arr, value) {
+    const lastIndx = arr.length - 1;
+    if (arr[lastIndx] === value) return lastIndx;
+
+    let low = 0,
+        high = arr.length - 1;
+    while (high >= low) {
+        let mid = Math.floor((low + high) / 2);
+        if (value < arr[mid]) {
+            high = mid - 1;
+        } else {
+            if (value === arr[mid] && value < arr[mid + 1]) return mid;
+            else low = mid + 1;
+        }
+    }
 }
 
 module.exports = sortedFrequency

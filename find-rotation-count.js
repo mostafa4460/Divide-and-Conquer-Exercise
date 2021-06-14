b36b62dba1e8e860a313f57fd84bbc3f232b62cf
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-06-13 21:05:08 -0400

    findRotationCount

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,21 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+    let start = 0,
+        end = arr.length - 1;
+    
+    if (arr[start] < arr[end]) return 0;
+
+    while (end >= start) {
+        let mid = Math.floor((start + end) / 2);
+        if (arr[mid] > arr[mid + 1]) {
+            return mid + 1;
+        } else if (arr[start] <= arr[mid]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+
+    return -1;
 }
 
 module.exports = findRotationCount
